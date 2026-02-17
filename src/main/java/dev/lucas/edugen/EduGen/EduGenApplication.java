c37a223c996f078f870fee2aca9526b11819a1b9
Lucasstq <fit.luk@outlook.com>
2026-02-17 09:39:22 -0300

    first commit

---
+++ src/main/java/dev/lucas/edugen/EduGen/EduGenApplication.java
@@ -0,0 +1,13 @@
+package dev.lucas.edugen.EduGen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduGenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduGenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/lucas/edugen/EduGen/EduGenApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.lucas.edugen.EduGen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduGenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
