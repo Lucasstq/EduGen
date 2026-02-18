a6a96cf82ad88116e64582966b72bffa03257b2d
Lucasstq <fit.luk@outlook.com>
2026-02-18 09:36:47 -0300

    feat: Adiciona mapeamento de entidade User para DTO e DTO para entidade User.

---
+++ src/main/java/dev/lucas/edugen/EduGen/mapper/UserMapper.java
@@ -0,0 +1,29 @@
+package dev.lucas.edugen.EduGen.mapper;
+
+import dev.lucas.edugen.EduGen.domain.User;
+import dev.lucas.edugen.EduGen.dtos.request.RegisterUserRequest;
+import dev.lucas.edugen.EduGen.dtos.response.user.UserResponse;
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class UserMapper {
+
+    public static User toEntity(RegisterUserRequest request) {
+        return User.builder()
+                .username(request.username())
+                .email(request.email())
+                .hashPassword(request.password())
+                .build();
+    }
+
+    public static UserResponse toResponse(User user) {
+        return UserResponse.builder()
+                .username(user.getUsername())
+                .email(user.getEmail())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt())
+                .build();
+    }
+
+
+}
