ae7bcb0f816bccbe81b42c704185e336978e66a8
Lucasstq <fit.luk@outlook.com>
2026-02-18 14:51:01 -0300

    feat: Adiciona querys especificas para serem usadas em services.

--- src/main/java/dev/lucas/edugen/EduGen/repository/WorksheetRepository.java
+++ src/main/java/dev/lucas/edugen/EduGen/repository/WorksheetRepository.java
@@ -1,9 +1,28 @@
 package dev.lucas.edugen.EduGen.repository;
 
+import dev.lucas.edugen.EduGen.domain.User;
 import dev.lucas.edugen.EduGen.domain.Worksheet;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @Repository
 public interface WorksheetRepository extends JpaRepository<Worksheet, Long> {
+
+    @Query("SELECT COUNT(w) FROM Worksheet w WHERE w.owner = :owner")
+    long countWorksheetByOwner(User owner);
+
+    /*
+        * Método para buscar as worksheets de um usuário específico, com paginação.
+     */
+    Page<Worksheet> findByOwner(User owner, Pageable pageable);
+
+    Optional<Worksheet> findByIdAndOwnerId(Long id, UUID ownerId);
+
+
 }
--- src/main/java/dev/lucas/edugen/EduGen/repository/WorksheetVersionRepository.java
+++ src/main/java/dev/lucas/edugen/EduGen/repository/WorksheetVersionRepository.java
@@ -4,6 +4,15 @@ import dev.lucas.edugen.EduGen.domain.WorksheetVersion;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Repository
 public interface WorksheetVersionRepository extends JpaRepository<WorksheetVersion, Long> {
+
+    /*
+        * Método para buscar uma versão específica de uma worksheet, garantindo que ela pertence ao usuário correto.
+     */
+    Optional<WorksheetVersion> findByIdAndWorksheetOwnerId(Long id, UUID ownerId);
+
 }
