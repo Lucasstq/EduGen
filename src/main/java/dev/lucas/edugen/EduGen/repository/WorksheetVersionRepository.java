ae0621e75f49d42aebb5ec90e352d0feaf141c5e
Lucasstq <fit.luk@outlook.com>
2026-02-19 17:00:05 -0300

    feat: queries para usuario.

--- src/main/java/dev/lucas/edugen/EduGen/repository/WorksheetVersionRepository.java
+++ src/main/java/dev/lucas/edugen/EduGen/repository/WorksheetVersionRepository.java
@@ -1,9 +1,13 @@
 package dev.lucas.edugen.EduGen.repository;
 
 import dev.lucas.edugen.EduGen.domain.WorksheetVersion;
+import dev.lucas.edugen.EduGen.domain.enums.Subject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -15,4 +19,9 @@ public interface WorksheetVersionRepository extends JpaRepository<WorksheetVersi
      */
     Optional<WorksheetVersion> findByIdAndWorksheetOwnerId(Long id, UUID ownerId);
 
+    List<WorksheetVersion> findTop3ByWorksheetOwnerIdOrderByCreatedAtDesc(UUID ownerId);
+
+    Page<WorksheetVersion> findByWorksheetOwnerId(UUID ownerId, Pageable pageable);
+
+    Page<WorksheetVersion> findByWorksheetOwnerIdAndWorksheetSubject(UUID ownerId, Subject subject, Pageable pageable);
 }
